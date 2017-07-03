723e76a994219faab0be9fbd47ad2bdfe7cf198b
Xenos Konstantinos <mr.xkon@gmail.com>
2017-07-03 22:19:53 +0300

    admin menu rework
    reworked the admin menu to comply with wordpress admin theme

--- js/scripts.js
+++ js/scripts.js
@@ -2,66 +2,6 @@
 
     $(document).ready(function () {
 
-        // Appointments Menu
-
-        hasapptax = $('body').hasClass('taxonomy-tsm-appointments-taxonomy');
-
-        if (hasapptax) {
-            $('#toplevel_page_tattoo-shop-manager').addClass('wp-menu-open');
-            $('#toplevel_page_tattoo-shop-manager').addClass('wp-has-current-submenu');
-            $('#toplevel_page_tattoo-shop-manager').removeClass('wp-not-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').addClass('wp-menu-open');
-            $('a.toplevel_page_tattoo-shop-manager').addClass('wp-has-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').removeClass('wp-not-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').attr('aria-haspopup', 'false');
-            $('a:contains("Appointment Types")').parent().addClass("current");
-        }
-
-        // Employees Menu
-
-        hasstafftax = $('body').hasClass('taxonomy-tsm-employees-taxonomy');
-
-        if (hasstafftax) {
-            $('#toplevel_page_tattoo-shop-manager').addClass('wp-menu-open');
-            $('#toplevel_page_tattoo-shop-manager').addClass('wp-has-current-submenu');
-            $('#toplevel_page_tattoo-shop-manager').removeClass('wp-not-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').addClass('wp-menu-open');
-            $('a.toplevel_page_tattoo-shop-manager').addClass('wp-has-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').removeClass('wp-not-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').attr('aria-haspopup', 'false');
-            $('a:contains("Job Positions")').parent().addClass("current");
-        }
-
-        // Inks Menu
-
-        hasinkstax = $('body').hasClass('taxonomy-tsm-inks-taxonomy');
-
-        if (hasinkstax) {
-            $('#toplevel_page_tattoo-shop-manager').addClass('wp-menu-open');
-            $('#toplevel_page_tattoo-shop-manager').addClass('wp-has-current-submenu');
-            $('#toplevel_page_tattoo-shop-manager').removeClass('wp-not-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').addClass('wp-menu-open');
-            $('a.toplevel_page_tattoo-shop-manager').addClass('wp-has-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').removeClass('wp-not-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').attr('aria-haspopup', 'false');
-            $('a:contains("Ink Companies & Sets")').parent().addClass("current");
-        }
-
-        // Needles Menu
-
-        hasneedlestax = $('body').hasClass('taxonomy-tsm-needles-taxonomy');
-
-        if (hasneedlestax) {
-            $('#toplevel_page_tattoo-shop-manager').addClass('wp-menu-open');
-            $('#toplevel_page_tattoo-shop-manager').addClass('wp-has-current-submenu');
-            $('#toplevel_page_tattoo-shop-manager').removeClass('wp-not-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').addClass('wp-menu-open');
-            $('a.toplevel_page_tattoo-shop-manager').addClass('wp-has-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').removeClass('wp-not-current-submenu');
-            $('a.toplevel_page_tattoo-shop-manager').attr('aria-haspopup', 'false');
-            $('a:contains("Needle Companies")').parent().addClass("current");
-        }
-
         // Flatpickr Settings
 
         $('#tsm_client_meta_birthday').flatpickr({
