53d9092f22df18c5d1fb35d5b55ffceedeb54d11
Xenos Konstantinos <mr.xkon@gmail.com>
2017-07-08 14:26:16 +0300

    for v1.0.4 - shortcode fixes
    shortcode fixes

--- tattoo-shop-manager.php
+++ tattoo-shop-manager.php
@@ -157,7 +157,7 @@ function tattoo_shop_manager_clients_shortcode(){
   $total_clients = new WP_Query(array(
       'post_type' => 'tsm-clients'
   ));
-  echo '<span class="tsm_total_clients">' . $total_clients->found_posts . '</span>';
+  return '<span class="tsm_total_clients">' . $total_clients->found_posts . '</span>';
 
 }
 
@@ -169,7 +169,7 @@ function tattoo_shop_manager_appointments_shortcode(){
   $total_appointments = new WP_Query(array(
       'post_type' => 'tsm-appointments'
   ));
-  echo '<span class="tsm_total_appointments">' . $total_appointments->found_posts . '</span>';
+  return '<span class="tsm_total_appointments">' . $total_appointments->found_posts . '</span>';
 
 }
 
